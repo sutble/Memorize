a30214fa93b8c0551d0cbc6b58fe5b70a45f5533
Aditya Garg <adityagarg@berkeley.edu>
2020-07-03 20:51:42 -0700

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Adi on 7/3/20.
+//  Copyright © 2020 Adi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
